8790e4d59ee01a3182e804a2a8ebabffe96dd6b6
NriotHrreion <nriot233@outlook.com>
2025-09-29 16:20:20 +0800

    feat: Use `SecureRandom` to improve random security

--- core/src/main/java/net/opanel/utils/Utils.java
+++ core/src/main/java/net/opanel/utils/Utils.java
@@ -6,9 +6,8 @@ import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.StandardOpenOption;
 import java.security.MessageDigest;
-import java.util.Arrays;
+import java.security.SecureRandom;
 import java.util.Base64;
-import java.util.Random;
 import java.util.stream.Stream;
 import java.util.zip.GZIPInputStream;
 
@@ -177,10 +176,9 @@ public class Utils {
     public static String generateRandomCharSequence(int length) {
         final String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@$";
         StringBuilder result = new StringBuilder();
-        Random rand = new Random();
+        SecureRandom rand = new SecureRandom();
         while(result.length() < length) {
-            int charIndex = Math.round(chars.length() * rand.nextFloat());
-            if(charIndex == chars.length()) charIndex--;
+            int charIndex = rand.nextInt(chars.length());
             result.append(chars.charAt(charIndex));
         }
         return result.toString();
