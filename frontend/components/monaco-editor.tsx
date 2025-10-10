ec71ff777907435a8aa9a8f58184a719f865fe18
NriotHrreion <nriot233@outlook.com>
2025-10-10 17:34:15 +0800

    fix(monaco): Code highlight for json is not working

--- frontend/components/monaco-editor.tsx
+++ frontend/components/monaco-editor.tsx
@@ -1,10 +1,30 @@
 "use client";
 
+import { useEffect } from "react";
 import { Editor, type EditorProps } from "@monaco-editor/react";
 import { loader } from "@monaco-editor/react";
 import * as monaco from "monaco-editor";
 
 export default function MonacoEditor(props: EditorProps) {
+  useEffect(() => {
+    if(typeof window === "undefined") return;
+
+    (window as any).MonacoEnvironment = {
+      getWorker: (_workerId: never, label: string) => {
+        switch(label) {
+          case "json":
+            return new Worker(
+              new URL("monaco-editor/esm/vs/language/json/json.worker", import.meta.url)
+            );
+          default:
+            return new Worker(
+              new URL("monaco-editor/esm/vs/editor/editor.worker", import.meta.url)
+            );
+        }
+      }
+    };
+  }, []);
+
   loader.config({ monaco });
   return <Editor {...props}/>;
 }
