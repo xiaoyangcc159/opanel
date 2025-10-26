450edce502c330f57b86378d0f163eca10e87fe2
NriotHrreion <nriot233@outlook.com>
2025-10-26 21:46:28 +0800

    feat: Search player list

--- frontend/app/panel/players/page.tsx
+++ frontend/app/panel/players/page.tsx
@@ -2,7 +2,7 @@
 
 import type { Player, PlayersResponse } from "@/lib/types";
 import { useEffect, useMemo, useState } from "react";
-import { Contact, UserPen, Users } from "lucide-react";
+import { Contact, Search, UserPen, Users } from "lucide-react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { DataTable } from "@/components/data-table";
 import { sendGetRequest, toastError } from "@/lib/api";
@@ -12,12 +12,14 @@ import { Button } from "@/components/ui/button";
 import { WhitelistSheet } from "./whitelist-sheet";
 import { setWhitelistEnabled } from "./player-utils";
 import { emitter } from "@/lib/emitter";
+import { InputGroup, InputGroupAddon, InputGroupInput } from "@/components/ui/input-group";
 
 export default function Players() {
   const [players, setPlayers] = useState<Player[]>([]);
   const [maxPlayerCount, setMaxPlayerCount] = useState<number>(0);
   const [isWhitelistEnabled, setWhitelistEnabledState] = useState(false);
   const [currentTab, setCurrentTab] = useState<string>("player-list");
+  const [searchString, setSearchString] = useState<string>("");
   const nonBannedPlayers = useMemo(() => players.filter(({ isBanned }) => !isBanned), [players]);
   const bannedPlayers = useMemo(() => players.filter(({ isBanned }) => isBanned), [players]);
 
@@ -50,7 +52,7 @@ export default function Players() {
       className="flex flex-col gap-3">
       <span className="text-sm text-muted-foreground">点击玩家名以进行更多操作。</span>
       <Tabs defaultValue="player-list" onValueChange={setCurrentTab}>
-        <div className="flex justify-between items-center max-sm:flex-col-reverse max-sm:items-start max-sm:gap-2">
+        <div className="flex justify-between items-center max-lg:flex-col-reverse max-lg:items-start max-lg:gap-2">
           <TabsList className="[&>*]:cursor-pointer">
             <TabsTrigger value="player-list">
               {`玩家列表 (${players.filter(({ isOnline }) => isOnline).length} / ${maxPlayerCount})`}
@@ -59,45 +61,57 @@ export default function Players() {
               {`封禁列表 (${players.filter(({ isBanned }) => isBanned).length})`}
             </TabsTrigger>
           </TabsList>
-          {
-            isWhitelistEnabled
-            ? (
-              <WhitelistSheet
-                onDisableWhitelist={() => setWhitelistEnabledState(false)}
-                asChild>
+          <div className="flex gap-2 max-sm:flex-col max-sm:items-start">
+            <InputGroup>
+              <InputGroupAddon>
+                <Search />
+              </InputGroupAddon>
+              <InputGroupInput
+                value={searchString}
+                placeholder="搜索玩家..."
+                onChange={(e) => setSearchString(e.target.value)}/>
+            </InputGroup>
+            {
+              isWhitelistEnabled
+              ? (
+                <WhitelistSheet
+                  onDisableWhitelist={() => setWhitelistEnabledState(false)}
+                  asChild>
+                  <Button
+                    variant="outline"
+                    className="cursor-pointer">
+                    <UserPen />
+                    编辑白名单
+                  </Button>
+                </WhitelistSheet>
+              )
+              : (
                 <Button
                   variant="outline"
-                  className="cursor-pointer">
-                  <UserPen />
-                  编辑白名单
+                  className="cursor-pointer"
+                  onClick={async () => {
+                    await setWhitelistEnabled(true);
+                    await fetchPlayerList();
+                    /**
+                     * We need to set the state manually here
+                     * because the whitelist state fetched from the server has a delay.
+                     */
+                    setWhitelistEnabledState(true);
+                  }}>
+                  <Contact />
+                  启用白名单
                 </Button>
-              </WhitelistSheet>
-            )
-            : (
-              <Button
-                variant="outline"
-                className="cursor-pointer"
-                onClick={async () => {
-                  await setWhitelistEnabled(true);
-                  await fetchPlayerList();
-                  /**
-                   * We need to set the state manually here
-                   * because the whitelist state fetched from the server has a delay.
-                   */
-                  setWhitelistEnabledState(true);
-                }}>
-                <Contact />
-                启用白名单
-              </Button>
-            )
-          }
+              )
+            }
+          </div>
         </div>
         <TabsContent value="player-list">
           <DataTable
             columns={playerColumns}
             data={[
-              ...nonBannedPlayers.filter(({ isOnline }) => isOnline),
-              ...nonBannedPlayers.filter(({ isOnline }) => !isOnline)
+              ...nonBannedPlayers.filter(({ name, isOnline }) => name.toLowerCase().includes(searchString.toLowerCase()) && isOnline),
+              ...nonBannedPlayers.filter(({ name, isOnline, isOp }) => name.toLowerCase().includes(searchString.toLowerCase()) && !isOnline && isOp),
+              ...nonBannedPlayers.filter(({ name, isOnline, isOp }) => name.toLowerCase().includes(searchString.toLowerCase()) && !isOnline && !isOp)
             ]}
             pagination
             fallbackMessage="暂无玩家"/>
@@ -105,7 +119,7 @@ export default function Players() {
         <TabsContent value="banned-list">
           <DataTable
             columns={bannedColumns}
-            data={bannedPlayers}
+            data={bannedPlayers.filter(({ name }) => name.toLowerCase().includes(searchString.toLowerCase()))}
             pagination
             fallbackMessage="暂无玩家"/>
         </TabsContent>
