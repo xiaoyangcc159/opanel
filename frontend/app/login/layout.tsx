43388263a6992622af6b8283620b7110a49c598f
NriotHrreion <nriot233@outlook.com>
2026-01-21 13:13:12 +0800

    wip: Add banner picture for login page

--- frontend/app/login/layout.tsx
+++ frontend/app/login/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import DefaultBanner from "@/assets/images/default-banner.png";
 
 export const metadata: Metadata = {
   title: "OPanel"
@@ -10,8 +11,11 @@ export default function LoginLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="w-[100vw] h-[100vh] flex justify-center items-center">
-      {children}
+    <div className="w-[100vw] h-[100vh] flex">
+      <div className="flex-2/5 h-full bg-cover bg-center max-lg:hidden" style={{ backgroundImage: `url(${DefaultBanner.src})` }}/>
+      <div className="flex-3/5 border-l shadow-2xl z-10 flex justify-center items-center">
+        {children}
+      </div>
     </div>
   );
 }
